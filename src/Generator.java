6162d694e7569dacbc93ee8111423d182f2a512f
vicky <tyagivivek528@gmail.com>
2025-03-28 16:45:47 +0530

    password generated without jumbling

--- src/Generator.java
+++ src/Generator.java
@@ -4,15 +4,36 @@ public class Generator {
 //uppercase = 65-90 -> 1
     //lowercase = 97-122 ->1
     //numbers = 48-57 -> 2
-    //characters = 33-47 -> 2
+    //characters = 33-47 -> 1
+        Random rand = new Random();
+        int count = 0;
+    int[][] asciiCodes = {
+        {33,48,2,0},//spc characters
+        {48,58,2,0},//numbers
+        {65, 91,1,0},//uppercase
+        {97,123,1,0},//lowercase
+};
     public String generate() {
         StringBuilder sb = new StringBuilder();
+        int n = asciiCodes.length;
 
-        Random rand = new Random();
-        int num = rand.nextInt(65,91);
-        System.out.println(num);
+        while(count<n){
+            int[] ranges = asciiCodes[count];
+            int rngInit = ranges[0];
+            int rngFinal = ranges[1];
+        if(ranges[3]>=ranges[2]){
+            count++;
+            continue;
+        }
+        int num = rand.nextInt(rngInit,rngFinal);
+        char c = (char) (num);
+//        System.out.println(c);
+        sb.append(c);
+        asciiCodes[count][3]++;
 
+        }
         //logic
+        System.out.println(sb);
         return sb.toString();
     }
 }
